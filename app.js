01f227923a0ebfe62a123e9a185606ed5ca34f26
Blas Zorrilla <blaszorrilla03@gmail.com>
2025-02-22 20:41:58 -0300

    Se agregan las funcionalidades del juego

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Escribe un nombre en el campo por favor");
+        return;
+    }
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+        
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `<li>El amigo sorteado es: ${amigoSorteado}</li>`;
+    } else {
+        alert("No hay nombres en la lista para sortear.");
+    }
+}
+function reiniciar(){
+    amigos = [];
+    let lista = document.getElementById("listaAmigos");
+    let resultado = document.getElementById("resultado");
+    lista.innerHTML = "";
+    resultado.innerHTML = "";
+}
